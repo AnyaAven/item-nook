a500a2f8d1cc53ef1766b1e2de835d73817cda8b
Anya Aven <leafeon96@gmail.com>
2024-09-05 16:33:33 -0400

    Factory: Add team and users

--- database/seeders/DatabaseDataSeeder.php
+++ database/seeders/DatabaseDataSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Item;
 use App\Models\Space;
+use App\Models\Team;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,14 +16,26 @@ class DatabaseDataSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        $team = Team::factory()
+            ->state(['name' => 'First Factory Team'])
+            ->create();
 
-        // Empty space
-        Space::factory()->count(1)->create();
+        User::factory(3)
+            ->create()
+            ->each(function(User $user) use ($team) {
+                $user->teams()->attach($team, ['role' => 'member']);
+                $user->update(['current_team_id' => $team->id]);
+            });
+
+        //         Empty space
+        Space::factory()
+            ->for($team)
+            ->create();
 
         Space::factory()
             ->state(['name' => 'Factory Space'])
             ->has(Item::factory()->count(6))
+            ->for($team)
             ->create();
     }
 }
